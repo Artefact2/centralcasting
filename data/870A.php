8c4869395d8cf267841d145a1d7b02e751fff472
Artefact2 <artefact2@gmail.com>
2016-12-26 13:46:08 +0100

    Invoke consistency.

--- data/870A.php
+++ data/870A.php
@@ -6,7 +6,7 @@ return new NamedTable("870A", "Brain Damage Consequence", DiceRoller::from("d8")
 	"1" => "-".Roll("d3")." Intelligence",
 	"2" => "All skills lose one rank",
 	"3" => Invoker("649B"),
-	"4" => Invoker("649A"),
+	"4" => Invoker("649"),
 	"5" => "-".Roll("d3")." Dexterity",
 	"6" => "Increase one skill by ".Roll("d8")." rank(s), all others lose d6 ranks",
 	"7-8" => Repeater(Roll("d3+1"), Invoker("870A")),
