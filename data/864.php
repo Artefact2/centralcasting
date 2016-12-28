9a02f787f1920004fbc6698d24ea029f4ed1353f
Artefact2 <artefact2@gmail.com>
2016-12-28 16:47:10 +0100

    Fix typo in 864.

--- data/864.php
+++ data/864.php
@@ -29,9 +29,9 @@ return new NamedTable("864", "Deity", DiceRoller::from("d20+CuMod"), [
 	"25-" => [ "Decadent version of god:", function(State $s) {
 		$reroll = Roll("d20+CuMod", $s);
 		if($reroll >= 25) {
-			$s->invokeTable("864", new RerollFilter(DiceRoller::from("d20+CuMod", function($r) {
+			$s->invokeTable("864", new RerollFilter(DiceRoller::from("d20+CuMod"), function($r) {
 				return $r < 25;
-			})));
+			}));
 			return "Disguised evil version of god:";
 		}
 		$s->invokeTable("864", new TrivialRoller($reroll));
